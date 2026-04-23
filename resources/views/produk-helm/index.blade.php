cb6edd3f6f6857876c9b312e134c1270529ea39f
Iren Maharani Ruben <irenmaharani641@gmail.com>
2026-04-23 20:14:11 +0700

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/produk-helm/index.blade.php
@@ -0,0 +1,6 @@
+<x-app>
+
+    <x-slot:title>produk-helm</x-slot>
+
+    <h1 class="fw-bold">Data Helm</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('produk-helm', function () {
+    return view('produk-helm.index', ['title' => 'Helm']);
+});
+
+Route::get('produk-helm/create', function () {
+    return view('produk-helm.create', ['title' => 'Create Helm']);
+});
